b98a03c72a6d9194a4372cdbacc74d1af9f72eb2
kimgiraffe <kimgiraffe@naver.com>
2022-02-13 13:10:30 +0900

    220213

---
+++ 20254.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int UR, TR, UO, TO;
+	cin >> UR >> TR >> UO >> TO;
+	cout << 56 * UR + 24 * TR + 14 * UO + 6 * TO;
+
+	return 0;
+}
