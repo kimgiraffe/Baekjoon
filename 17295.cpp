6337b8d12ef1e75564ec02c7387516421b9350bc
kimgiraffe <kimgiraffe@naver.com>
2022-04-06 11:24:58 +0900

    update .gitignore

--- 17295.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void) {
-	printf("Avengers: Endgame");
-
-	return 0;
-}
--- 24078.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main(void)
-{
-	int X;
-	scanf("%d", &X);
-	printf("%d", X % 21);
-	return 0;
-}
