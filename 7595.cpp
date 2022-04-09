c2a7851cf0612a54c2f13c0f8afd3782f8c8e790
kimgiraffe <kimgiraffe@naver.com>
2022-04-09 11:59:54 +0900

    add 7595.cpp

---
+++ 7595.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	while (true) {
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				cout << "*";
+			}
+			cout << "\n";
+		}
+	}
+
+	return 0;
+}
