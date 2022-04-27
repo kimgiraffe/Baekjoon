32f91e2b47c75e4443a4c010da94190f1a896a56
kimgiraffe <kimgiraffe@naver.com>
2022-04-27 12:13:50 +0900

    add 24751.cpp

---
+++ 24751.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	double a;
+	cin >> a;
+	cout.fixed;
+	cout.precision(10);
+	cout << 100 / a << '\n';
+	cout << 100 / (100 - a);
+
+	return 0;
+}
