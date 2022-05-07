d8c3e644e73f6496e723fbc9e4d761c8d4268e99
kimgiraffe <kimgiraffe@naver.com>
2022-05-07 18:16:48 +0900

    add 24072.cpp

---
+++ 24072.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (a <= c && b > c) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+
+
+	return 0;
+}
