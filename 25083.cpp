f28b279038895eecaed9e53e68099feabe620254
kimgiraffe <kimgiraffe@naver.com>
2022-05-23 14:12:18 +0900

    220523

---
+++ 25083.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	printf("         ,%c'\"%d\n", 'r', 7);
+	printf("%c`-_   ,'  ,/\n", 'r');
+	printf(" \\. \". %c_%c'\n", 'L', 'r');
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
