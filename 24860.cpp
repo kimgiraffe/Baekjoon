98f2d96d4f0fcdb7567183a492597832c2ee0287
kimgiraffe <kimgiraffe@naver.com>
2022-06-03 14:30:24 +0900

    220603

---
+++ 24860.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	long long vk, jk, vy, jy, vh, dh, jh;
+	cin >> vk >> jk >> vy >> jy >> vh >> dh >> jh;
+
+	cout << vk * jk * vh * dh * jh + vy * jy * vh *dh * jh;
+
+	return 0;
+}
