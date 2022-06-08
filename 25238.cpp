abeb9fe650caf9d826ca69d86bae782b27945716
kimgiraffe <kimgiraffe@naver.com>
2022-06-08 12:09:27 +0900

    220608

---
+++ 25238.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	cin >> a >> b;
+	if (a * (100 - b) / 100 >= 100) {
+		cout << 0;
+	}
+	else {
+		cout << 1;
+	}
+
+	return 0;
+}
