e8be7a3b747e868d1acd0b416ea2d6e9f1bb90de
kimgiraffe <kimgiraffe@naver.com>
2022-06-13 01:11:54 +0900

    220613

---
+++ 15921.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int N, a, sum = 0;
+	cin >> N;
+	if (N == 0) {
+		cout << "divide by zero";
+	}
+	else {
+		while (N--) {
+			cin >> a;
+		}
+		cout << "1.00";
+	}
+
+	return 0;
+}
