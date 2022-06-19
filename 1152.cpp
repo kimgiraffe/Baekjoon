b847e868ebe7accb36915d2e3ebc45518c4cdd46
kimgiraffe <kimgiraffe@naver.com>
2022-06-19 21:08:04 +0900

    220619

--- 1152.cpp
+++ 1152.cpp
@@ -11,7 +11,7 @@ int main(void)
 
 	int len = str.length();
 
-	if (str[0] == ' ') {
+	if (str[0] == ' ' || str[0] == '\0') {
 		cnt--;
 	}
 
@@ -23,5 +23,6 @@ int main(void)
 	}
 
 	cout << cnt + 1;
+
 	return 0;
 }
