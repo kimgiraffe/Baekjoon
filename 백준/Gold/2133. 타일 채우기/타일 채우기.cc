22bbf498a109f534c8434f2f9941fcf651640c60
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 11:28:08 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int N;
+int dp[30];
+int Fill(int n)
+{
+	dp[0] = 1;
+	dp[1] = 3;
+	if (n % 2 != 0) {
+		return 0;
+	}
+	for (int i = 2; i <= N / 2; i++) {
+		dp[i] = dp[i - 1] * 4 - dp[i - 2];
+	}
+	return dp[N / 2];
+}
+
+int main(void)
+{
+	cin >> N;
+	cout << Fill(N) << '\n';
+	return 0;
+}
