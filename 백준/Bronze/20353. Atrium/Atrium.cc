99e22af86a023d97eae01942db982fdc33955197
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 13:49:54 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void)
+{
+	long long a;
+	cin >> a;
+	cout << fixed;
+	cout.precision(10);
+	cout << sqrt(a) * 4;
+
+	return 0;
+}
