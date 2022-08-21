55448a130a8076022678799f8ff2212b0cfede03
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 12:36:38 +0900

    [Silver IV] Title: 큐, Time: 340 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <queue>
+ 
+using namespace std;
+ 
+int main() {
+    queue <int> q;
+    int num;
+ 
+    cin >> num;
+ 
+    for (int i = 0; i < num; i++) {
+        string com;
+        cin >> com;
+ 
+        if (com == "push") {
+            int com_end;
+            cin >> com_end;
+ 
+            q.push(com_end);
+        }
+        else if (com == "pop") {
+            if (q.empty())
+                cout << "-1" << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (com == "size") {
+            cout << q.size() << '\n';
+        }
+        else if (com == "empty") {
+            if (q.empty())
+                cout << "1" << '\n';
+            else
+                cout << "0" << '\n';
+        }
+        else if (com == "front") {
+            if(q.empty())
+                cout << "-1" << '\n';
+            else
+                cout << q.front() << '\n';
+        }
+        else if (com == "back") {
+            if (q.empty())
+                cout << "-1" << '\n';
+            else
+                cout << q.back() << '\n';
+        }
+    }
+}
