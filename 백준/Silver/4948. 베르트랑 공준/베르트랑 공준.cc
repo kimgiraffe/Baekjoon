7cfade542039821cb10c2db753725ee0214fab8f
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 12:03:39 +0900

    [Silver III] Title: 베르트랑 공준, Time: 132 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,316 @@
+#include<stdio.h>
+
+int isPrime(int num) {
+	if (num == 1) {
+		return 0;
+	}
+	else if (num % 2 == 0 && num != 2) {
+		return 0;
+	}
+	else if (num % 3 == 0 && num != 3) {
+		return 0;
+	}
+	else if (num % 5 == 0 && num != 5) {
+		return 0;
+	}
+	else if (num % 7 == 0 && num != 7) {
+		return 0;
+	}
+	else if (num % 11 == 0 && num != 11) {
+		return 0;
+	}
+	else if (num % 13 == 0 && num != 13) {
+		return 0;
+	}
+	else if (num % 17 == 0 && num != 17) {
+		return 0;
+	}
+	else if (num % 19 == 0 && num != 19) {
+		return 0;
+	}
+	else if (num % 23 == 0 && num != 23) {
+		return 0;
+	}
+	else if (num % 29 == 0 && num != 29) {
+		return 0;
+	}
+	else if (num % 31 == 0 && num != 31) {
+		return 0;
+	}
+	else if (num % 31 == 0 && num != 31) {
+		return 0;
+	}
+	else if (num % 37 == 0 && num != 37) {
+		return 0;
+	}
+	else if (num % 41 == 0 && num != 41) {
+		return 0;
+	}
+	else if (num % 43 == 0 && num != 43) {
+		return 0;
+	}
+	else if (num % 47 == 0 && num != 47) {
+		return 0;
+	}
+	else if (num % 53 == 0 && num != 53) {
+		return 0;
+	}
+	else if (num % 59 == 0 && num != 59) {
+		return 0;
+	}
+	else if (num % 61 == 0 && num != 61) {
+		return 0;
+	}
+	else if (num % 67 == 0 && num != 67) {
+		return 0;
+	}
+	else if (num % 71 == 0 && num != 71) {
+		return 0;
+	}
+	else if (num % 73 == 0 && num != 73) {
+		return 0;
+	}
+	else if (num % 79 == 0 && num != 79) {
+		return 0;
+	}
+	else if (num % 83 == 0 && num != 83) {
+		return 0;
+	}
+	else if (num % 89 == 0 && num != 89) {
+		return 0;
+	}
+	else if (num % 97 == 0 && num != 97) {
+		return 0;
+	}
+	else if (num % 101 == 0 && num != 101) {
+		return 0;
+	}
+	else if (num % 103 == 0 && num != 103) {
+		return 0;
+	}
+	else if (num % 107 == 0 && num != 107) {
+		return 0;
+	}
+	else if (num % 109 == 0 && num != 109) {
+		return 0;
+	}
+	else if (num % 113 == 0 && num != 113) {
+		return 0;
+	}
+	else if (num % 127 == 0 && num != 127) {
+		return 0;
+	}
+	else if (num % 131 == 0 && num != 131) {
+		return 0;
+	}
+	else if (num % 137 == 0 && num != 137) {
+		return 0;
+	}
+	else if (num % 139 == 0 && num != 139) {
+		return 0;
+	}
+	else if (num % 149 == 0 && num != 149) {
+		return 0;
+	}
+	else if (num % 151 == 0 && num != 151) {
+		return 0;
+	}
+	else if (num % 157 == 0 && num != 157) {
+		return 0;
+	}
+	else if (num % 163 == 0 && num != 163) {
+		return 0;
+	}
+	else if (num % 167 == 0 && num != 167) {
+		return 0;
+	}
+	else if (num % 173 == 0 && num != 173) {
+		return 0;
+	}
+	else if (num % 179 == 0 && num != 179) {
+		return 0;
+	}
+	else if (num % 181 == 0 && num != 181) {
+		return 0;
+	}
+	else if (num % 191 == 0 && num != 191) {
+		return 0;
+	}
+	else if (num % 193 == 0 && num != 193) {
+		return 0;
+	}
+	else if (num % 197 == 0 && num != 197) {
+		return 0;
+	}
+	else if (num % 199 == 0 && num != 199) {
+		return 0;
+	}
+	else if (num % 211 == 0 && num != 211) {
+		return 0;
+	}
+	else if (num % 223 == 0 && num != 223) {
+		return 0;
+	}
+	else if (num % 227 == 0 && num != 227) {
+		return 0;
+	}
+	else if (num % 229 == 0 && num != 229) {
+		return 0;
+	}
+	else if (num % 233 == 0 && num != 233) {
+		return 0;
+	}
+	else if (num % 239 == 0 && num != 239) {
+		return 0;
+	}
+	else if (num % 241 == 0 && num != 241) {
+		return 0;
+	}
+	else if (num % 251 == 0 && num != 251) {
+		return 0;
+	}
+	else if (num % 257 == 0 && num != 257) {
+		return 0;
+	}
+	else if (num % 263 == 0 && num != 263) {
+		return 0;
+	}
+	else if (num % 269 == 0 && num != 269) {
+		return 0;
+	}
+	else if (num % 271 == 0 && num != 271) {
+		return 0;
+	}
+	else if (num % 277 == 0 && num != 277) {
+		return 0;
+	}
+	else if (num % 281 == 0 && num != 281) {
+		return 0;
+	}
+	else if (num % 283 == 0 && num != 283) {
+		return 0;
+	}
+	else if (num % 293 == 0 && num != 293) {
+		return 0;
+	}
+	else if (num % 307 == 0 && num != 307) {
+		return 0;
+	}
+	else if (num % 311 == 0 && num != 311) {
+		return 0;
+	}
+	else if (num % 313 == 0 && num != 313) {
+		return 0;
+	}
+	else if (num % 317 == 0 && num != 317) {
+		return 0;
+	}
+	else if (num % 331 == 0 && num != 331) {
+		return 0;
+	}
+	else if (num % 337 == 0 && num != 337) {
+		return 0;
+	}
+	else if (num % 347 == 0 && num != 347) {
+		return 0;
+	}
+	else if (num % 349 == 0 && num != 349) {
+		return 0;
+	}
+	else if (num % 353 == 0 && num != 353) {
+		return 0;
+	}
+	else if (num % 359 == 0 && num != 359) {
+		return 0;
+	}
+	else if (num % 367 == 0 && num != 367) {
+		return 0;
+	}
+	else if (num % 373 == 0 && num != 373) {
+		return 0;
+	}
+	else if (num % 379 == 0 && num != 379) {
+		return 0;
+	}
+	else if (num % 383 == 0 && num != 383) {
+		return 0;
+	}
+	else if (num % 389 == 0 && num != 389) {
+		return 0;
+	}
+	else if (num % 397 == 0 && num != 397) {
+		return 0;
+	}
+	else if (num % 401 == 0 && num != 401) {
+		return 0;
+	}
+	else if (num % 409 == 0 && num != 409) {
+		return 0;
+	}
+	else if (num % 419 == 0 && num != 419) {
+		return 0;
+	}
+	else if (num % 421 == 0 && num != 421) {
+		return 0;
+	}
+	else if (num % 431 == 0 && num != 431) {
+		return 0;
+	}
+	else if (num % 433 == 0 && num != 433) {
+		return 0;
+	}
+	else if (num % 439 == 0 && num != 439) {
+		return 0;
+	}
+	else if (num % 443 == 0 && num != 443) {
+		return 0;
+	}
+	else if (num % 449 == 0 && num != 449) {
+		return 0;
+	}
+	else if (num % 457 == 0 && num != 457) {
+		return 0;
+	}
+	else if (num % 461 == 0 && num != 461) {
+		return 0;
+	}
+	else if (num % 463 == 0 && num != 463) {
+		return 0;
+	}
+	else if (num % 467 == 0 && num != 467) {
+		return 0;
+	}
+	else if (num % 479 == 0 && num != 479) {
+		return 0;
+	}
+	else if (num % 487 == 0 && num !=487) {
+		return 0;
+	}
+	else if (num % 491 == 0 && num != 491) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+
+}
+
+
+int main(void)
+{
+	int n, count = 0;
+	while(1) {
+		scanf("%d", &n);
+		if (n == 0)
+			break;
+		for (int i = n + 1; i <= 2 * n; i++) {
+			if (isPrime(i) == 1) {
+				count++;
+			}
+		}
+		printf("%d\n", count);
+		count = 0;
+	}
+
+	return 0;
+}
