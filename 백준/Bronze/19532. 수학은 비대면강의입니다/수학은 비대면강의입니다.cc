9dad91d0b9b45d1d264a8b29444934bdeb58d153
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 13:48:21 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c, d, e, f, x, y;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	for (int i = -999; i <= 999; i++) {
+		for (int j = -999; j <= 999; j++) {
+			if (a * i + b * j == c && d * i + e * j == f) {
+				x = i;
+				y = j;
+			}
+		}
+	}
+	printf("%d %d", x, y);
+	return 0;
+}
