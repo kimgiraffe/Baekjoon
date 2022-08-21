9456db385b9f891564aff71ab91a3ca56b09e722
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 13:13:09 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+	long long N;
+	cin >> N;
+	cout << fixed;
+	cout.precision(0);
+	cout << floor(sqrt(N));
+	
+
+	return 0;
+}
