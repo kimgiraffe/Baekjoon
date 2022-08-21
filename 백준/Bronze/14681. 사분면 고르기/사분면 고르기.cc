dfa4b19617cbbcbc3d37cb342605be88f6277988
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 13:22:10 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (x > 0) {
+		if (y > 0) {
+			printf("1\n");
+		}
+		else {
+			printf("4\n");
+		}
+	}
+	else {
+		if (y > 0) {
+			printf("2\n");
+		}
+		else {
+			printf("3\n");
+		}
+	}
+
+	return 0;
+}
