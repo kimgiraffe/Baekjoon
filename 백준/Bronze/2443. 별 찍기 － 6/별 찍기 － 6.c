b95445630fe47b21e4b93f7e3763fcfa74a403f7
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 11:35:25 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 3164 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--) {
+		for (int j = n; j > i; j--) {
+			printf(" ");
+		}	
+		for (int k = 2 * i - 1; k > 0; k--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
