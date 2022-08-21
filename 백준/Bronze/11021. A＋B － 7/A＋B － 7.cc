11ccc0f8b1a6da0bd22f10f458a4fae27a7589ca
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 12:48:39 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int t, i;
+	int a, b, sum = 0;
+	scanf("%d", &t);
+	for (i = 0; i < t; i++) {
+		scanf("%d", &a);
+		scanf("%d", &b);
+		sum = a + b;
+		printf("Case #%d: %d\n", i + 1, sum);
+		sum = 0;
+	}
+
+	return 0;
+}
