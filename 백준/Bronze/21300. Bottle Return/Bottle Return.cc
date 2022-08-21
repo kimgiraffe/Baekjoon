be31dfe132b5073739ad8b5ed5f826a428c084a6
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-08-21 13:50:33 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int b, m, w, sd, s, wa;
+	cin >> b >> m >> w >> sd >> s >> wa;
+	cout << (b + m + w + sd + s + wa) * 5;
+
+	return 0;
+}
