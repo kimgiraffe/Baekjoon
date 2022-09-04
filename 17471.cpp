ca206f72aafb2602656cca37bb63f4e40baebe6f
kimgiraffe <kimgiraffe@naver.com>
2022-09-04 15:26:11 +0900

    220904

--- 17471.cpp
+++ 17471.cpp
@@ -11,10 +11,9 @@ int N;
 int p[11], check1[11], check2[11];
 vector<int> node[11];
 vector<int> Left, Right;
-bool visited[11];
 int res = INT_MAX;
 
-int cal(vector<int> v, int num)
+int cal(vector<int> v, int num) // 나누어진 구역이 연결되어있는지 확인하는 함수
 {
 	int cnt = 1;
 	queue<int> q;
@@ -50,22 +49,22 @@ void dfs(int L, int cnt)
 				Right.push_back(i);
 			}
 		}
-		if (Left.size() == 0 || Right.size() == 0) return;
+		if (Left.size() == 0 || Right.size() == 0) return;	//양쪽 지역구 중 하나가 비어있으면 무시
 
 		int ans = 0;
-		ans += cal(Left, 1);
-		ans += cal(Right, 0);
+		ans += cal(Left, 1);	//1번 선거구
+		ans += cal(Right, 0);	//2번 선거구
 
-		if (ans == N) {
+		if (ans == N) {	//전체 구역의 개수와 같다면
 			int p_left = 0, p_right = 0;
-			for (int i = 0; i < Left.size(); ++i) {
+			for (int i = 0; i < Left.size(); ++i) {	//1번 지역구의 인구 수 합
 				p_left += p[Left[i]];
 			}
-			for (int i = 0; i < Right.size(); ++i) {
+			for (int i = 0; i < Right.size(); ++i) {	//2번 지역구의 인구 수 합
 				p_right += p[Right[i]];
 			}
-			int val = abs(p_left - p_right);
-			if (res > val) res = val;
+			int val = abs(p_left - p_right); //두 선거구의 인구 차이
+			if (res > val) res = val; // 최소값 갱신
 		}
 		Left.clear();
 		Right.clear();
