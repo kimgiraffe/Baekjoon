0c3aca49f46bc7dc46544d9b0fb6f40b7e22f14a
unknown <kimgiraffe@naver.com>
2022-09-10 21:37:14 +0900

    220910

---
+++ 20232.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int year;
+	cin >> year;
+	switch (year) {
+	case 1995:
+		cout << "ITMO";
+		break;
+	case 1996:
+		cout << "SPbSU";
+		break;
+	case 1997:
+		cout << "SPbSU";
+		break;
+	case 1998:
+		cout << "ITMO";
+		break;
+	case 1999:
+		cout << "ITMO";
+		break;
+	case 2000:
+		cout << "SPbSU";
+		break;
+	case 2001:
+		cout << "ITMO";
+		break;
+	case 2002:
+		cout << "ITMO";
+		break;
+	case 2003:
+		cout << "ITMO";
+		break;
+	case 2004:
+		cout << "ITMO";
+		break;
+	case 2005:
+		cout << "ITMO";
+		break;
+	case 2006:
+		cout << "PetrSU, ITMO";
+		break;
+	case 2007:
+		cout << "SPbSU";
+		break;
+	case 2008:
+		cout << "SPbSU";
+		break;
+	case 2009:
+		cout << "ITMO";
+		break;
+	case 2010:
+		cout << "ITMO";
+		break;
+	case 2011:
+		cout << "ITMO";
+		break;
+	case 2012:
+		cout << "ITMO";
+		break;
+	case 2013:
+		cout << "SPbSU";
+		break;
+	case 2014:
+		cout << "ITMO";
+		break;
+	case 2015:
+		cout << "ITMO";
+		break;
+	case 2016:
+		cout << "ITMO";
+		break;
+	case 2017:
+		cout << "ITMO";
+		break;
+	case 2018:
+		cout << "SPbSU";
+		break;
+	case 2019:
+		cout << "ITMO";
+		break;
+	default:
+		break;
+
+	}
+
+	return 0;
+}
