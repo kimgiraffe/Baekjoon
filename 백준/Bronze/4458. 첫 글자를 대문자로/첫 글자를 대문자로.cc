2409cb169c5742d56ba82af245c34538403c10a9
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-09-23 21:28:53 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// freopen("input.txt", "r", stdin);
+	int N;
+	string str;
+	cin >> N;
+	cin.ignore();
+	for (int i = 1; i <= N; i++) {
+		getline(cin, str);
+		if (str[0] >= 'a' && str[0] <= 'z') {
+			str[0] += ('A' - 'a');
+		}
+		cout << str << '\n';
+	}
+
+	return 0;
+}
