26f45671c27ab5db86d0df274eb06272233117ad
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2022-12-31 13:43:47 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 220 ms, Memory: 16680 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    // freopen("input.txt", "r", stdin);
+    int N,M;
+    map<string,string> m;
+    string a,b;
+
+    cin >> N >> M;
+
+    for(int i=0;i<N;i++){
+        cin >> a >> b;
+
+        m[a] = b;
+    }
+
+    for(int i=0;i<M;i++){
+        cin >> a;
+
+        cout << m[a] << '\n';
+    }
+
+    return 0;
+}
