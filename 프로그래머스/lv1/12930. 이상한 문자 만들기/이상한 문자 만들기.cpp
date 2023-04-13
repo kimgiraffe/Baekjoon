0255d6950e0523af54f812ac5b3f6f30d732a163
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2023-04-13 15:58:28 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int idx = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == ' '){
+            idx = 0;
+            continue;
+        }
+        if(idx % 2)
+            s[i] = tolower(s[i]);
+        else
+            s[i] = toupper(s[i]);
+        idx++;
+    }
+    return s;
+}
